6f478ebdaf0a772a53e0f62bc56850ea1f8c3830
anushasima <0939713580@ukr.net>
2025-02-02 19:23:28 +0200

    added code

---
+++ main.py
@@ -0,0 +1,29 @@
+import keyword
+import string
+
+def is_valid_variable_name(name):
+    if name in keyword.kwlist:
+        return False
+
+    if name.count('_') > 1:
+        return False
+
+    if any(char.isupper() for char in name):
+        return False
+
+    allowed_chars = set(string.ascii_lowercase + string.digits + '_')
+    if any(char not in allowed_chars for char in name):
+        return False
+
+    if name[0].isdigit():
+        return False
+
+    if not name:
+        return False
+
+    return True
+
+user_input = input("Введіть ім'я змінної: ").strip()
+
+print(is_valid_variable_name(user_input))
+
